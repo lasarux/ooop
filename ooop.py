9074a82ab6b012d86ab42ad0c1e2f4971536166a
Pedro Gracia <lasarux@gmail.com>
2010-12-28 12:36:21 +0100

    issue #10 fixed by simahawk

--- ooop.py
+++ ooop.py
@@ -565,7 +565,7 @@ class Data(object):
                         data[name] = self.__dict__[name]
                 elif ttype in ('one2many', 'many2many'):
                     if len(self.__dict__[name]) > 0:
-                        data[name] = [(6, 0, [i._ref for i in self.__dict__[name]])]
+                        data[name] = [(6, 0, [i.save() for i in self.__dict__[name]])]
                         # update __name and INSTANCES (cache)
                         self.__dict__['__%s' % name] = [i._ref for i in self.__dict__[name]] # REVIEW: two loops?
                         for i in self.__dict__[name]:
